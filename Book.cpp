abaffd4c71f9ce80f13ae92f0b69c52c34ac87db
Youssef W. Soliman <ywsoliman@gmail.com>
2022-04-26 03:08:46 +0200

    Added Book Class
    Implemented Book.cpp and Book.h

--- Book.cpp
+++ Book.cpp
@@ -1,1 +1,105 @@
 #include "Book.h"
+
+int Book::count = 0;
+
+Book::Book()
+{
+	count++;
+	title = "";
+	isbn = "";
+	id = count;
+	category = "";
+	averageRating = 0.0;
+	lastTotalRating = 0;
+	ratingCounts = 0;
+}
+
+Book::Book(string title, string isbn, string category) : title(title), isbn(isbn), category(category) {}
+
+Book::Book(const Book& book)
+{
+	title = book.title;
+	isbn = book.isbn;
+	id = book.id;
+	category = book.category;
+	averageRating = book.averageRating;
+}
+
+void Book::setTitle(string title)
+{
+	this->title = title;
+}
+string Book::getTitle()
+{
+	return title;
+}
+void Book::setISBN(string isbn)
+{
+	this->isbn = isbn;
+}
+string Book::getISBN()
+{
+	return isbn;
+}
+void Book::setId(int id)
+{
+	this->id = id;
+}
+int Book::getId()
+{
+	return id;
+}
+void Book::setCategory(string category)
+{
+	this->category = category;
+}
+string Book::getCategory()
+{
+	return category;
+}
+void Book::setAuthor(User user)
+{
+	author = user;
+}
+User Book::getAuthor()
+{
+	return author;
+}
+
+void Book::rateBook(int rating)
+{
+	lastTotalRating += rating;
+	ratingCounts++;
+	averageRating = lastTotalRating / ratingCounts;
+}
+
+bool Book::operator==(const Book& book)
+{
+	if (title == book.title && category == book.category && id == book.id && author == book.author)
+		return true;
+	return false;
+}
+
+ostream& operator<<(ostream& output, const Book& book)
+{
+	output << "========== Book " << book.id << " info ==========" << endl;
+	output << "Title: " << book.title << "\nISBN: " << book.isbn << "\nCategory: " << book.category << "\nAverage Rating: " << book.averageRating << endl;
+	output << "=====================================" << endl;
+	if (book.author.getName() != "") {
+		output << "Author of Book #" << book.id << ":" << endl;
+		output << book.author << endl;
+	}
+	return output;
+}
+
+istream& operator>>(istream& input, Book& book)
+{
+	if (book.id == 1)
+		cout << "Let's add a book" << endl;
+	else
+		cout << "Let's add another user" << endl;
+	cout << "Enter the user information in this order" << endl;
+	cout << "Title ISBN Category (Space separated)." << endl;
+	input >> book.title >> book.isbn >> book.category;
+	return input;
+}
--- Book.h
+++ Book.h
@@ -1,5 +1,38 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "User.h"
+
+using namespace std;
+
 class Book
 {
+private:
+	string title;
+	string isbn;
+	int id;
+	string category;
+	double averageRating;
+	int lastTotalRating;
+	int ratingCounts;
+	User author;
+public:
+	static int count;
+	Book();
+	Book(string title, string isbn, string category);
+	Book(const Book& book);
+	void setTitle(string title);
+	string getTitle();
+	void setISBN(string isbn);
+	string getISBN();
+	void setId(int id);
+	int getId();
+	void setCategory(string category);
+	string getCategory();
+	void setAuthor(User user);
+	User getAuthor();
+	void rateBook(int rating);
+	bool operator==(const Book& book);
+	friend ostream& operator<<(ostream& output, const Book& book);
+	friend istream& operator>>(istream& input, Book& book);
 };
-
